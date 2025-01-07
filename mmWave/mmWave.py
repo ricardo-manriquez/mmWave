9f5fdd28125c5bdff2314660e46326834d74f0ae
ricardo-manriquez <64954533+ricardo-manriquez@users.noreply.github.com>
2025-01-07 16:15:25 +0900

    New ROS 2 package

---
+++ mmWave/mmWave.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+import serial
+from std_msgs.msg import Float32, Bool, Int32, String, Float32MultiArray
+from geometry_msgs.msg import Point
+
+from commands import commands
+from util import checksum, check_pkt, fill_checksum
+import struct
+
+#from .mmwave_decoder import MMWaveDecoder
+#from .mmwave_defs import FilterCategories, TOPIC_GROUPS
+
+class MMWaveNode(Node):
+    def __init__(self, ser):
+        super().__init__('mmwave_node')
+
+        # init active filters
+        self.active_filters = set()  # null set means all topics are active
+
+        # create publishers
+        self._publishers = {
+            'heartrate': self.create_publisher(Float32, 'mmwave/heartrate', 10),
+            'heartrate_waveform': self.create_publisher(Float32MultiArray, 'mmwave/heartrate_waveform', 10),
+            'presence': self.create_publisher(Bool, 'mmwave/presence', 10),
+            'distance': self.create_publisher(Int32, 'mmwave/distance', 10),
+            'position': self.create_publisher(Point, 'mmwave/position', 10),
+            'movement': self.create_publisher(Int32, 'mmwave/movement', 10),
+            'activity': self.create_publisher(String, 'mmwave/activity', 10),
+            'breathing': self.create_publisher(String, 'mmwave/breathing', 10),
+            'respiratory_rate': self.create_publisher(Int32, 'mmwave/respiratory_rate', 10),
+            'respiratory_waveform': self.create_publisher(Float32MultiArray, 'mmwave/respiratory_waveform', 10),
+            'bed_status': self.create_publisher(String, 'mmwave/bed_status', 10),
+            'sleep_state': self.create_publisher(String, 'mmwave/sleep_state', 10),
+            'sleep_status': self.create_publisher(String, 'mmwave/sleep_status', 10),
+            'sleep_analysis': self.create_publisher(String, 'mmwave/sleep_analysis', 10),
+            'sleep_quality': self.create_publisher(String, 'mmwave/sleep_quality', 10),
+            'struggling': self.create_publisher(String, 'mmwave/struggling', 10),
+            'sleep_abnormality': self.create_publisher(String, 'mmwave/sleep_abnormality', 10),
+        }
+
+        # create parameter to set filters
+        self.declare_parameter('filters', '')
+
+        self.add_on_set_parameters_callback(self.check_filter_parameter)
+
+        self.cmd_dict = {}
+        self.cmds = {}
+        self.dev = None
+        self.con = None
+        self.callback = None
+        self._filter = []
+        self.stack = {}
+        for command in commands:
+            if not command.control in self.cmd_dict:
+                self.cmd_dict[command.control] = {}
+            self.cmd_dict[command.control][command.command] = command()
+            self.cmds[command.__name__] = self.cmd_dict[command.control][command.command]
+        
+        
+        # serial port
+        self.ser = ser
+        
+    def check_filter_parameter(self):
+        """check if the filter parameter has been updated"""
+        filters_str = self.get_parameter('filters').value
+        if filters_str:
+            new_filters = set(filters_str.split(','))
+            if new_filters != self.active_filters:
+                self.active_filters = new_filters
+                self.get_logger().info(f'Updated filters: {self.active_filters}')
+        return SetPArametersResult(successful=True)
+
+    def is_topic_active(self, topic_name: str) -> bool:
+        """check if a topic is active based on the current filters"""
+        # if no filters are active, all topics are active
+        if not self.active_filters or FilterCategories.ALL in self.active_filters:
+            return True
+            
+        # check if the topic is in any of the active filter categories
+        for filter_category in self.active_filters:
+            if filter_category in TOPIC_GROUPS and topic_name in TOPIC_GROUPS[filter_category]:
+                return True
+        return False
+
+    def interpret_package(self, pkt):
+        try:
+            cmd = self.cmd_dict[pkt[2].to_bytes()][pkt[3].to_bytes()]
+            response = None
+            if cmd.name() in self.stack:
+                self.stack[cmd.name()].set()
+                self.stack[cmd.name()] = cmd.process(pkt)
+            else:
+                if hasattr(cmd, "process"):
+                    response = cmd.process(pkt)
+                else:
+                    response = repr(pkt)
+            if self.callback:
+                if self._filter:
+                    if type(cmd) in self._filter:
+                        if response:
+                            self.callback(cmd, response)
+                else:
+                    if response:
+                        self.callback(cmd, response)
+        except:
+            print(pkt)
+            raise
+
+    def __del__(self):
+        if hasattr(self, 'ser') and self.ser.is_open:
+            self.ser.close()
+
+    def command_callback(self, cmd, response):
+        print(cmd, response)
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    ser = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        timeout=1
+    )
+    
+    node = MMWaveNode(ser)
+    
+    rclpy.spin(node)
+    while True:
+        rclpy.spin_once(node, timeout_sec=0)
+        r, w, x = select.select((ser),(),())
+        if ser in r:
+            b = ser.read(6)
+            if b[0:2] == b'\x53\x59':
+                m_len = struct.unpack(">h", b[4:6])[0] + 1
+                b += ser.read(m_len + 2)
+                if check_pkt(b):
+                    node.interpret_package(b)
+            else:
+                b = ser.read_until(b"\x54\x43")
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'mmWave'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'msg'), glob('msg/*.msg')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your@email.com',
+    description='MMWave radar package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mmwave_publisher = mmWave.mmWave:main',
+            'mmwave_subscriber = mmWave.subscriber_node:main',
+        ],
+    },
+)
